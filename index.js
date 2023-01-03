b2ae22a58bd28c435b37d60c63e9041413c27c58
Elena <tolyazoya1970@gmail.com>
2023-01-03 16:09:36 +0200

    Урок 13.12.22

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-console.log("work");
+console.log('work');
 
-const $wr = document.querySelector('[data-wr]')  
+const $wr = document.querySelector('[data-wr]');
 
-
-const getCatHTML = (cat) => { 
-	return ` 
-		<div class="card mb-3 mx-2" style="width: 15rem"> 
-			<img src="${cat.image}" class="card-img-top" alt="${cat.name}" /> 
-			<div class="card-body">
-			<h5 class="card-title">${cat.name}</h5>
-			<p class="card-text">
-				${cat.description}
-			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
-		</div>
+const getCatHTML = (cat) => `
+    <div class="card mb-3 mx-2" style="width: 15rem"> 
+	<img src="${cat.image}" class="card-img-top" alt="${cat.name}" /> 
+	<div class="card-body">
+		<h5 class="card-title">${cat.name}</h5>
+		<p class="card-text">
+			${cat.description}
+		</p>
+		<a href="#" class="btn btn-primary">Go somewhere</a>
 	</div>
-	`
-}
+ </div>
+	`;
 
-fetch('https://cats.petiteweb.dev/api/single/bronza2022/show/') 
-	.then((res) => res.json()) 
-	.then((data) => { 
-	
-		$wr.insertAdjacentHTML('afterbegin', data.map(cat => getCatHTML(cat)).join('') ) /
-		console.log({data}) 
-		})
-
+fetch('https://cats.petiteweb.dev/api/single/bronza2022/show/')
+  .then((res) => res.json())
+  .then((data) => {
+    $wr.insertAdjacentHTML('afterbegin', data.map((cat) => getCatHTML(cat)).join(''))
+		/ console.log({ data });
+  });
